51332630691880493a382cbb30d5d17bb5ed02f1
Samantha Laine <samanthalaine8@gmail.com>
2021-09-08 16:21:08 -0400

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn (addstring)
+        @knowledge << addstring
+    end
+    def knowledge
+        @knowledge
+    end
 end
